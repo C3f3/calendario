617ed6b1d8a557a24b76906051335bf1b2f7442a
Armando <armandoher01@gmail.com>
2020-06-05 01:40:38 -0300

    calendario refactorizado

--- index.php
+++ index.php
@@ -1,56 +1,59 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset='utf-8' />
-<link href='css/core/main.min.css' rel='stylesheet' />
-<link href='css/daygrid/main.min.css' rel='stylesheet' />
-<script src='js/core/main.min.js'></script>
-<script src='js/interaction/main.min.js'></script>
-<script src='js/daygrid/main.min.js'></script>
+  <meta charset="utf-8">
+  <title>Calendario</title>
+  <link href='packages/core/main.css' rel='stylesheet' />
+  <link href='packages/daygrid/main.css' rel='stylesheet' />
+  <link href='packages/timegrid/main.css' rel='stylesheet' />
+  <link rel="stylesheet" type="text/css" href="estilo.css">
+  <script src='packages/core/main.js'></script>
+  <script src='packages/interaction/main.js'></script>
+  <script src='packages/daygrid/main.js'></script>
+  <script src='packages/timegrid/main.js'></script>
+  <script src='packages/core/locales-all.js'></script>
+</head>
+<body>
+  <div id='calendar'></div>
 
-<script>
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
+  <script type="text/javascript">
+    document.addEventListener('DOMContentLoaded', function() {
+      var initialLocaleCode = 'es';
+      var calendarEl = document.getElementById('calendar');
 
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        locale: "es",
-      plugins: [ 'interaction', 'dayGrid' ],
-      //defaultDate: '2019-04-12',
+      var calendar = new FullCalendar.Calendar(calendarEl, {
+      plugins: [ 'interaction', 'dayGrid', 'timeGrid' ],
+      header: {
+        left: 'prev,next, today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      locale: initialLocaleCode,
+      
+      navLinks: true, // can click day/week names to navigate views
+      selectable: true,
+      selectMirror: true,
+      select: function(arg) {
+        var title = prompt('Event Title:');
+        if (title) {
+          calendar.addEvent({
+            title: title,
+            start: arg.start,
+            end: arg.end,
+            allDay: arg.allDay
+          })
+        }
+        calendar.unselect()
+      },
       editable: true,
       eventLimit: true, // allow "more" link when too many events
       events: 'lista_eventos.php',
-      //cache jason
-      extraParams: function() {
-        return {
-          cachebuster: new Date().valueOf()
-        };
-      }
     });
 
     calendar.render();
   });
 
-</script>
-<style>
-
-  body {
-    margin: 40px 10px;
-    padding: 0;
-    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-    font-size: 14px;
-  }
-
-  #calendar {
-    max-width: 900px;
-    margin: 0 auto;
-  }
-
-</style>
-</head>
-<body>
-
-  <div id='calendar'></div>
-
+  </script>
 </body>
 </html>
