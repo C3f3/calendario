bd359d4f1b56fb489b65f5220880bb6aa5a32002
Armando <armandoher01@gmail.com>
2020-06-04 21:22:03 -0300

    primer iteacion completa, funcionagit add -A

--- index.php
+++ index.php
@@ -1,4 +1,56 @@
-<?php
-require('conexion.php');
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8' />
+<link href='css/core/main.min.css' rel='stylesheet' />
+<link href='css/daygrid/main.min.css' rel='stylesheet' />
+<script src='js/core/main.min.js'></script>
+<script src='js/interaction/main.min.js'></script>
+<script src='js/daygrid/main.min.js'></script>
 
-?>
+<script>
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        locale: "es",
+      plugins: [ 'interaction', 'dayGrid' ],
+      //defaultDate: '2019-04-12',
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      events: 'lista_eventos.php',
+      //cache jason
+      extraParams: function() {
+        return {
+          cachebuster: new Date().valueOf()
+        };
+      }
+    });
+
+    calendar.render();
+  });
+
+</script>
+<style>
+
+  body {
+    margin: 40px 10px;
+    padding: 0;
+    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+    font-size: 14px;
+  }
+
+  #calendar {
+    max-width: 900px;
+    margin: 0 auto;
+  }
+
+</style>
+</head>
+<body>
+
+  <div id='calendar'></div>
+
+</body>
+</html>
